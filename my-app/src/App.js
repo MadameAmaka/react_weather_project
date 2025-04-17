49c0b9bb3e655b66a8532034be19bcfd0ca5b23c
Amaka Onwuli <amonwuli@gmail.com>
2025-04-17 14:21:14 +0100

    worked on the html component - added classNames and weather components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,11 @@
+import Weather from './Weather';
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-    <div className="App">
-     <h1>Weather App</h1>
+    <div className="Weather">
+     <Weather/>
      <footer> This project was coded by <a href='https://github.com/MadameAmaka' target='_blank'> Amaka Onwuli </a> and is open-sourced <a href='https://github.com/MadameAmaka/react_weather_project' target='_blank'> on Github. 
     </a></footer>
     </div></div>
