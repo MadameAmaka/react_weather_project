3720f5c91baf00af733ac685d95b5ab2f51da006
Amaka Onwuli <amonwuli@gmail.com>
2025-04-17 10:56:05 +0100

    Added the Github repo url

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,8 @@ export default function App() {
   return (
     <div className="App">
      <h1>Weather App</h1>
+     <footer> This project was coded by <a href='https://github.com/MadameAmaka' target='_blank'> Amaka Onwuli </a> and is <a href='https://github.com/MadameAmaka/react_weather_project' target='_blank'>
+    open-sourced </a>on Github. </footer>
     </div>
   );
 }
