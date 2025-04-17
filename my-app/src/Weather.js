dd0c311186e50a1b6fdffe4777e05c96629577d3
Amaka Onwuli <amonwuli@gmail.com>
2025-04-17 16:03:39 +0100

    Added basic styling in Weather.css

--- my-app/src/Weather.js
+++ my-app/src/Weather.js
@@ -8,24 +8,25 @@ export default function Weather(){
                 <div className="row">
                     <div className="col-9">
 
-                <input type="Search" placeholder="Enter a city..." className="form-control"/> 
+                <input type="Search" placeholder="Enter a city..." className="form-control" autoFocus="on"/> 
                 </div>
                 <div className="col-3">
-                <input type="Submit" value="Search" className="btn btn-primary"/>
+                <input type="Submit" value="Search" className="btn btn-primary" w-100/>
                 </div>
                 </div>
             </form>
             <h1> New York</h1>
             <ul>
-                <li> Wednesday</li>
-                <li>Mostly Cloudy</li>
+                <li> Thursday</li>
+                <li>Sunny</li>
             </ul>
-                <div className="row">
+                <div className="row mt-3">
                     <div className="col-6">
-                        <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="Sunny"/>
-                        6°C
+                        <div className="clearfix">
+                        <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="Sunny" className="float-left"/>
+                        <span className="temperature">6</span> <span className="unit">°C</span>
 
-                    </div>
+                    </div></div>
                     <div className="col-6">
                         <ul>
                             <li>
