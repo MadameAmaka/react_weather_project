f634f54baf9bbb1c957d98da8904e9bcbc4dcd06
Amaka Onwuli <amonwuli@gmail.com>
2025-04-17 13:31:39 +0100

    Added bootstrap

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+    <div className="App">
      <h1>Weather App</h1>
-     <footer> This project was coded by <a href='https://github.com/MadameAmaka' target='_blank'> Amaka Onwuli </a> and is <a href='https://github.com/MadameAmaka/react_weather_project' target='_blank'>
-    open-sourced </a>on Github. </footer>
-    </div>
+     <footer> This project was coded by <a href='https://github.com/MadameAmaka' target='_blank'> Amaka Onwuli </a> and is open-sourced <a href='https://github.com/MadameAmaka/react_weather_project' target='_blank'> on Github. 
+    </a></footer>
+    </div></div>
   );
 }
