b762009398fb03a3af3f58941772020421a6df1a
Amaka Onwuli <amonwuli@gmail.com>
2025-04-21 19:58:11 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
     <div className="Weather">
-     <Weather defaultCity="Ghana" />
+     <Weather defaultCity="New York" />
      <footer> This project was coded by <a href='https://github.com/MadameAmaka' target='_blank' rel="noreferrer"> Amaka Onwuli </a> and is open-sourced <a href='https://github.com/MadameAmaka/react_weather_project' target='_blank' rel="noreferrer" > on Github. 
     </a></footer>
     </div></div>
