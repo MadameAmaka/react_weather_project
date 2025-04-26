a8c645623c74dfc02ce616848afa9fb1d19667e4
Amaka Onwuli <amonwuli@gmail.com>
2025-04-26 12:48:12 +0100

    Update WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -5,11 +5,11 @@ import axios from "axios";
 export default function WeatherForecast(props){
     let[loaded, setLoaded] = useState(false);
     let[forecast, setForecast] = useState(null)
-    let[city, setCity]=useState(null);
+    //let[city, setCity]=useState(props.defaultCity);
     
     function displayForecast(response){
        setForecast(response.data.daily);
-       setCity (response.data.city);
+       //setCity(response.data.city);
        setLoaded(true);
 
     }
@@ -40,9 +40,8 @@ export default function WeatherForecast(props){
      
     } else{
         let apiKey = "2f43379oac5f7ffe8tde5aff442f0cdb";
+        let city = props.data.city;
         let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
         
         axios.get(apiUrl).then(displayForecast);
-
-        return null;
     }}
