eeebe52eab7b093b686387abf19ca568292979c4
Amaka Onwuli <amonwuli@gmail.com>
2025-04-27 23:40:49 +0100

    updated weather components to be SEO compliant

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,38 +1,38 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherTemperature from "./WeatherTemperature";
+import "./WeatherInfo.css"
 
 export default function WeatherInfo(props){
     return(
         <div className="weatherInfo">
-            <h1> {props.data.city}</h1>
-                <ul>
+            <div className="row">
+                <div className="col-6">
+                    <h1> {props.data.city}</h1> <ul>
                     <li> <FormattedDate date={props.data.date}/>
 
                     </li>
                     <li className="text-capitalize">{props.data.description}</li>
+                    
+                    <li>
+                        Humidity: <strong>
+                        {props.data.humidity}%</strong>
+                    </li>
+                    
+                    <li>
+                        Wind:<strong>{props.data.wind} mph</strong>
+                        </li>
                 </ul>
-                    <div className="row mt-3">
-                        <div className="col-6">
-                            <div className="clearfix">
-                                <img src={props.data.icon}alt={props.data.description}className="float-left"/>
-                                <WeatherTemperature celsius={props.data.temperature}/>
-                        </div></div>
-                        <div className="col-6">
-                            <ul>
-                               
-                                <li>
-                                    Humidity: {props.data.humidity}%
-                                </li>
-                                <li>
-                                    Wind:{props.data.wind} mph
-                                </li>
-    
-                            </ul>
-                        </div>
-    
+
+                </div>
+                
+                <div className="col-lg-6 col-md-6 col-sm-8">
+                    <div className="temperature-container d-flex justify-content-end">
+                        <img src={props.data.icon}alt={props.data.description} className="float-left"/>  <WeatherTemperature celsius={props.data.temperature}/>
                     </div>
+                </div>
             
+        </div>
         </div>
 
     );
